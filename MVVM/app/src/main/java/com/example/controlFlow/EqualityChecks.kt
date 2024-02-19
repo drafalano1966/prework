a1b8e325e83e15255dbcf37d20123e3f3ed1d00a
drafalano1966 <drafalano@gmail.com>
2024-02-19 12:46:27 -0500

    Additional examples
    Adding additional examples

---
+++ MVVM/app/src/main/java/com/example/controlFlow/EqualityChecks.kt
@@ -0,0 +1,12 @@
+package com.example.controlFlow
+
+fun main() {
+
+
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+    println(authors == writers)   // 1
+    println(authors === writers)  // 2
+
+}
