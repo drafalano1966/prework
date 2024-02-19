2e488aa17f48874cd84f971bb4eba8b487d165f0
drafalano1966 <drafalano@gmail.com>
2024-02-19 12:59:39 -0500

    Revert "Additional examples"
    This reverts commit a1b8e325e83e15255dbcf37d20123e3f3ed1d00a.

--- MVVM/app/src/main/java/com/example/controlFlow/ConditionalExpression.kt
+++
@@ -1,9 +0,0 @@
-package com.example.controlFlow
-
-fun main() {
-
-    fun max(a: Int, b: Int) = if (a > b) a else b         // 1
-
-    println(max(99, -42))
-
-}
--- MVVM/app/src/main/java/com/example/introduction/VarargFunctions.kt
+++ MVVM/app/src/main/java/com/example/introduction/VarargFunctions.kt
@@ -1,18 +1,6 @@
 package com.example.introduction
 
 fun main(){
-
-
-    val d: Int  // 1
-
-    d = if (someCondition()) {
-        1   // 2
-    } else {
-        2   // 2
-    }
-
-    println("this is the value of d: ${d}")
-
     fun printAll(vararg messages: String) {                            // 1
         for (m in messages) println(m)
     }
@@ -31,5 +19,3 @@ fun main(){
     }
     log("Hello", "Hallo", "Salut", "Hola", "你好")
 }
-
-fun someCondition() = true
